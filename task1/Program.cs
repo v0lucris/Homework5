426c6f9ec4c013dd9f8141fd6e6c75fda20028cc
Tati <tanushka-p.94@mail.ru>
2022-09-03 22:20:36 +0300

    save 1 task1

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+//k1 * x -y + b1 = 0
+//k2 * x - y + b2 =0
+//k2 * x - k1 * x - b1 + b2 = 0
+// x = (b1 - b2)/(k2 - k1)
+// y = k1 * (b1 - b2)/(k2 - k1) + b1
+Double k1 = new Random().Next(1,10);
+Double b1 = new Random().Next(1,10);
+Double k2 = new Random().Next(1,10);
+Double b2 = new Random().Next(1,10);
+Double x = (b1 - b2)/(k2 - k1);
+Double y = k1 * (b1 - b2)/(k2 - k1) + b1;
+
+Console.WriteLine($"k1 = {k1}; b1 = {b1}; k2 = {k2}; b2 = {b2};");
+Console.WriteLine($"точкa пересечения двух прямых: ({x};{y})");
