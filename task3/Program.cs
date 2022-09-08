f03900831799e9d37166861d6797b2a087273b3e
Tati <tanushka-p.94@mail.ru>
2022-09-09 02:16:37 +0300

    save1 task3

---
+++ task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Написать программу копирования массива
+
+int[] array = new int[10];
+int arrayCopy;
+
+Console.Write("Your array : [ ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 10);
+    Console.Write(array[i] + "  ");
+}
+Console.Write("]");
+Console.WriteLine();
+Console.Write("   Copy : [ ");
+for (int i = 0; i < array.Length; i++)
+{
+    arrayCopy = array[i];
+    Console.Write(arrayCopy + " ");
+   
+}
+Console.Write("]");
