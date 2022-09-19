46937054c15d80058e20416a3071ba76d2492955
Tati <tanushka-p.94@mail.ru>
2022-09-19 21:50:03 +0300

    save2 task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -10,6 +10,7 @@ Double k2 = new Random().Next(1,10);
 Double b2 = new Random().Next(1,10);
 Double x = (b1 - b2)/(k2 - k1);
 Double y = k1 * (b1 - b2)/(k2 - k1) + b1;
-
+if (k1 == k2) Console.WriteLine("Прямые параллельны и не имеют точек пересечения");
+if (k1 / k2 == 1 / 1) Console.WriteLine("Прямые совпадают ");
 Console.WriteLine($"k1 = {k1}; b1 = {b1}; k2 = {k2}; b2 = {b2};");
 Console.WriteLine($"точкa пересечения двух прямых: ({x};{y})");
